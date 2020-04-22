f61a1327b3c842313a0a125fde49d5264d58cff5
weronisian <weronika.na98@gmail.com>
2020-04-22 14:01:33 +0200

    Added minimax algorithm

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.awt.*;
 import java.util.Random;
 import java.util.Scanner;
@@ -5,8 +6,9 @@ import java.util.Scanner;
 public class Game {
     IEvaluationFunction function;
     private Board board;
-    private Point lastField;
+//    private Point lastField;
     private char lastPlayer;
+    private int nextMoveLocation;
     private int MODE;
     private int depth;
     Scanner scanner;
@@ -14,8 +16,9 @@ public class Game {
     public Game(int mode, int depth, IEvaluationFunction function){
         this.function = function;
         this.board = new Board();
-        this.lastField = new Point(0, 0);
+//        this.lastField = new Point(0, 0);
         this.lastPlayer = 'x';
+        this.nextMoveLocation = 0;
         this.MODE = mode;
         this.depth = depth;
         scanner = new Scanner(System.in);
@@ -27,38 +30,40 @@ public class Game {
             case 0:
                 board.displayBoard();
                 while (board.isEmptyField()) {
-                    lastPlayer = setPlayer(lastPlayer);
+                    lastPlayer = getOtherPlayer(lastPlayer);
                     humanMove();
                     board.displayBoard();
-                    if (checkIfWin(lastPlayer, board, lastField)) {
+                    if (checkIfWin(lastPlayer, board)) {
                         displayScores();
                         break;
                     }
                 }
+                if(!board.isEmptyField())
+                    System.out.println("Draw!");
                 break;
 
             case 1:
                 board.displayBoard();
                 while(board.isEmptyField()){
-                    lastPlayer = setPlayer(lastPlayer);
+                    lastPlayer = getOtherPlayer(lastPlayer);
                     humanMove();
                     board.displayBoard();
-                    if(checkIfWin(lastPlayer, board, lastField)) {
+                    if(checkIfWin(lastPlayer, board)) {
                         System.out.println("You Win!");
                         break;
                     }
 
-                    lastPlayer = setPlayer(lastPlayer);
+                    lastPlayer = getOtherPlayer(lastPlayer);
                     AIMove();
                     board.displayBoard();
-                    if(checkIfWin(lastPlayer, board, lastField)) {
+                    if(checkIfWin(lastPlayer, board)) {
                         System.out.println("AI Wins!");
                         break;
                     }
-
-//                    {System.out.println("Draw!");break;}
+//                    System.out.println("Draw!");
 
                 }
+                break;
         }
     }
 
@@ -69,7 +74,7 @@ public class Game {
             System.out.println("Invalid move.\n\nYour move - columns 0 to 6: ");
             move = scanner.nextInt();
         }
-        while(!makeMove(move, lastPlayer, board, lastField)) {
+        while(!makeMove(move, lastPlayer, board)) {
             System.out.println("Select other column");
             move = scanner.nextInt();
         }
@@ -77,77 +82,168 @@ public class Game {
 
     public void AIMove(){
         char player = lastPlayer;
-        Point field = lastField;
         Board board = new Board(this.board);
 
-        int bestEvaluation = Integer.MIN_VALUE;
-        int bestCol = 0;
-        for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
-            int res = minimax(0, i, true, player, board, field);
-            if(bestEvaluation < res) {
-                bestEvaluation = res;
-                bestCol = i;
-            }
-            System.out.println("Score for column " + i + " = " + res);
-        }
-//        Random random = new Random();
-//        int rand = random.nextInt(7);
-        makeMove(bestCol, lastPlayer, this.board, lastField);
+//        int bestEvaluation = Integer.MIN_VALUE;
+//        int bestCol = 0;
+//        for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
+//            makeMove(i, player,board);
+            int res = minimax(0, true, player, board);
+//            if(bestEvaluation < res) {
+//                bestEvaluation = res;
+//                bestCol = i;
+//            }
+//            System.out.println("Score for column " + i + " = " + res);
+//        }
+
+        System.out.println("Next move: " + nextMoveLocation);
+        makeMove(nextMoveLocation, lastPlayer, this.board);
     }
 
-    private boolean makeMove(int col, char lastPlayer, Board board, Point lastField){
+    private boolean makeMove(int col, char lastPlayer, Board board){
         int field = board.firstEmptyInCol(col);
         if(field != -1) {
             board.fields[field][col] = lastPlayer;
-            lastField.x = col;
-            lastField.y = field;
         } else {
             return false;
         }
         return true;
     }
 
-        public int minimax(int level, int col, boolean isMax, char player, Board board, Point lastField) {
-//            board.displayBoard();
-//            if (level == depth) {
-//                if(checkIfWin(lastPlayer, board, lastField))
-//                    return 1;
-//                char tmp = lastPlayer;
-//                if(checkIfWin(setPlayer(tmp), board, lastField))
-//                    return -1;
-            if (level == depth || !board.isEmptyField()) {
-                return function.evalueStatus(board);
-            }
-
-            // If current move is maximizer, find the maximum attainable
-            // value
-            if (isMax) {
-                int max = Integer.MIN_VALUE;
-                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
-//                    Board temp = new Board(board);
-                    makeMove(i, player, board, lastField);
-                    max = Math.max(max, minimax(level + 1, i, false, setPlayer(player), board, lastField));
-//                    if (res > max)
-//                        max = res;
-                }
-                return max;
-            }
-                // Else (If current move is Minimizer), find the minimum
-                // attainable value
-            else {
-                int min = Integer.MAX_VALUE;
-                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
-                    Board temp = new Board(board);
-                    makeMove(i, player, temp, lastField);
-                    min = Math.min(min,  minimax(level + 1, i, true, setPlayer(player), temp, lastField));
-//                    if (res < min)
-//                        min = res;
-                }
-                return min;
-            }
+    public int minimax(int level, boolean isMax, char player, Board board) {
+        board.displayBoard();
+        if(level == depth || !board.isEmptyField()){
+//            if(checkIfWin('x', board))
+//                return 10;
+//            if (checkIfWin('o', board))
+//                return -10;
+//            return 0;
+            return function.evalueStatus(board);
         }
 
-    private boolean checkIfWin(char lastPlayer, Board board, Point lastField){
+//        for(int j = 0; j < Board.BOARD_COLUMNS; j++) {
+            if (isMax) {
+                System.out.println("TERAZ MAX");
+                int maxScore = Integer.MIN_VALUE;
+                int value = 0;
+
+                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
+                    Board temp = new Board(board);
+                    if (makeMove(i, player, temp)) {
+                        value = minimax(level + 1, false, getOtherPlayer(player), temp);
+                    }
+//                    if(level == 0){
+                        System.out.println("Score for location "+i+" = "+value);
+                        if(value > maxScore)
+                            nextMoveLocation = i;
+//                    }
+                    maxScore = Math.max(value, maxScore);
+                }
+
+                System.out.println("Max value: " + maxScore);
+                return maxScore;
+            } else {
+                System.out.println("TERAZ MIN");
+                int minScore = Integer.MAX_VALUE;
+                int value = 0;
+
+                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
+                    Board temp = new Board(board);
+                    if (makeMove(i, player, temp)) {
+                        value = minimax(level + 1, true, getOtherPlayer(player), temp);
+                    }
+                    minScore = Math.min(value, minScore);
+                }
+
+                System.out.println("Min value: " + minScore);
+                return minScore;
+            }
+//        }
+//        return -1;
+    }
+
+
+
+//    public int minimax(int level, boolean isMax, char player, Board board) {
+//        board.displayBoard();
+//        if(level == depth || !board.isEmptyField()){
+////            if(checkIfWin('x', board))
+////                return 10;
+////            if (checkIfWin('o', board))
+////                return -10;
+////            return 0;
+//            return function.evalueStatus(board);
+//        }
+//
+//        if(isMax){
+//            System.out.println("TERAZ MAX");
+//            int value = Integer.MIN_VALUE;
+//
+//            for(int i=0; i<Board.BOARD_COLUMNS; i++){
+//                Board temp = new Board(board);
+//                if(makeMove(i, player, temp)) {
+//                    value = Math.max(value, minimax(level + 1, false, getOtherPlayer(player), temp));
+//                }
+//            }
+//            System.out.println("Max value: "+ value);
+//            return value;
+//        }
+//        else{
+//            System.out.println("TERAZ MIN");
+//            int value = Integer.MAX_VALUE;
+//            for(int i=0; i<Board.BOARD_COLUMNS; i++){
+//                Board temp = new Board(board);
+//                if(makeMove(i, player, temp)) {
+//                    value = Math.min(value, minimax(level + 1, true, getOtherPlayer(player), temp));
+//                }
+//            }
+//            System.out.println("Min value: "+ value);
+//            return value;
+//        }
+//
+//    }
+
+//
+//        public int minimax(int level, boolean isMax, char player, Board board) {
+////          System.out.println("Player " + player);
+////            board.displayBoard();
+////            if (level == depth) {
+////                if(checkIfWin(lastPlayer, board))
+////                    return 1;
+////                if(checkIfWin(getOtherPlayer(player), board))
+////                    return -1;
+//            if (level == depth || !board.isEmptyField()) {
+//                return function.evalueStatus(board);
+////                return 0;
+//            }
+//
+//            // If current move is maximizer, find the maximum attainable value
+//            if (isMax) {
+//                int max = Integer.MIN_VALUE;
+//                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
+////                    Board temp = new Board(board);
+//                    makeMove(i, player, board);
+//                    max = Math.max(max, minimax(level + 1, false, getOtherPlayer(player), board));
+////                    if (res > max)
+////                        max = res;
+//                }
+//                return max;
+//            }
+//                // Else (If current move is Minimizer), find the minimum attainable value
+//            else {
+//                int min = Integer.MAX_VALUE;
+//                for (int i = 0; i < Board.BOARD_COLUMNS; i++) {
+//                    Board temp = new Board(board);
+//                    makeMove(i, player, temp);
+//                    min = Math.min(min,  minimax(level + 1, true, getOtherPlayer(player), temp));
+////                    if (res < min)
+////                        min = res;
+//                }
+//                return min;
+//            }
+//        }
+
+    private boolean checkIfWin(char lastPlayer, Board board){
 //        System.out.println("Lastfield: "+ lastField.x + " " + lastField.y);
 //        // vertical
 //        int minRow = Math.max(0, lastField.y - 3);
@@ -231,6 +327,13 @@ public class Game {
         else
             lastPlayer = 'x';
         return lastPlayer;
+    }
+
+    private char getOtherPlayer(char player){
+        if(player == 'x')
+            return 'o';
+        else
+            return 'x';
     }
 
     public void displayScores(){
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args){
 
-        int depth = 6;
+        int depth = 5;
 
-        Game game = new Game(0, depth, new EvaluationFunctionWinLose());
-//        Game game1 = new Game(1, depth, new EvaluationFunctionWinLose());
+//        Game game = new Game(0, depth, new EvaluationFunctionWinLose());
+        Game game1 = new Game(1, depth, new EvaluationFunctionWinLose());
 
 
 
